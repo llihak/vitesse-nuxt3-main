3fe9fa5c6510e8b101c36359a6bf6896ba558050
keith hill <keith.hill@metastock.com>
2022-11-12 10:48:10 -0700

    initial commit before sass

---
+++ components/Golden.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="golden bg-slate-200">
+    <article>article</article>
+    <aside>aside</aside>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  .wtw {
+    line-height: 1.1;
+    ul {
+      line-height: 2.22;
+      li {
+        line-height: 3.333;
+      }
+    }
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Golden></Golden>
+    <!-- <Logos mb-6 /> -->
+    <!-- <Suspense>
+      <PageView />
+      <template #fallback>
+        <div op50 italic>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense> -->
+    <!-- <InputEntry /> -->
+  </div>
+</template>
